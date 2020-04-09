6d74e060d2b6066f61da6b3abf0e52e66eb67e15
J.M. Ashfiqur Rahman Anik <anik.ku.cse@gmail.com>
2020-04-09 18:32:00 +0600

    added data model and fake data resource

---
+++ app/src/main/java/com/example/videolistwithrecyclerview/models/MediaObject.kt
@@ -0,0 +1,8 @@
+package com.example.videolistwithrecyclerview.models
+
+data class MediaObject(
+    val title: String,
+    val media_url: String,
+    val thumbnail: String,
+    val description: String
+)
